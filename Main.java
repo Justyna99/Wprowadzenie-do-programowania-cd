5b95047b56263aef02ff932c1ae6f9d108407a73
Justyna99 <justynaulicka6@gmail.com>
2022-08-18 18:48:59 +0200

    Dodałam nowa klase main "Hej Justyna" i "Super grupa"

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hej Justyna");
-        System.out.println("Super grupa");
+        String name = "Justyna";
+        int age = 22;
+        char znak = 'a';
+        int x = 10;
+        int y = 20;
+        int c = x + y;
+       // System.out.println("Hej "+ name + " ktora ma " + age);
+        System.out.println(c);
+
+
+
+
+
+
     }
 }
